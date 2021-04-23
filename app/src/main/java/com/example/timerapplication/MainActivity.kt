f5525dfa0c204ec04c518d343e2d5c8171794725
BearGom2 <31925642+sjcalaa@users.noreply.github.com>
2021-04-23 13:07:25 +0900

    주석 추가

--- app/src/main/java/com/example/timerapplication/MainActivity.kt
+++ app/src/main/java/com/example/timerapplication/MainActivity.kt
@@ -36,15 +36,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        //초기화 버튼 클릭
         stopBtn.setOnClickListener {
             reset()
         }
 
     }
 
+    //timer의 중심 기능 함수
     private fun start() {
-        if (time > 0) {
-            timerTask = fixedRateTimer(period = 10) {
+        if (time > 0) { //time이 0보다 작으면 실행이 불가
+            timerTask = fixedRateTimer(period = 10) { //기본 rate를 10ms로 설정
                 time--
                 var sec = time / 100
                 val mil = time % 100
@@ -57,6 +59,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //reload 함수에서만 쓰이는 시간 추가 기능을 함수 내부에 추가
     private fun reload(t: Int) {
         time += t
         var sec = time / 100
@@ -71,6 +74,7 @@ class MainActivity : AppCompatActivity() {
         timerTask?.cancel()
     }
 
+    //타이머 초기화, 모든 변수 및 Text 초기화
     private fun reset() {
         timerTask?.cancel()
 
@@ -82,6 +86,7 @@ class MainActivity : AppCompatActivity() {
         startBtn.text = "시작하기"
     }
 
+    //Text 대입과 조건문이 reload와 start 함수에서 반복되어 중복되는 구문을 묶어서 함수로 선언
     private fun setText(min: Int, sec: Int, mil: Int) {
         minTv.text = "$min"
         secTv.text = ":${sec % 60}"
